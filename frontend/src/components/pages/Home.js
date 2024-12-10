7fb5dc78f1b900b764546f30edb622c75001e8f3
cavareal <al.cavaree@gmail.com>
2024-12-10 16:03:58 +0100

    fix: import from Home.js

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import Header from "../molecules/Header.js";
-import Hello from "../molecules/Hello.js";
-import Project from "../molecules/Project.js";
-import YourComponent from "../molecules/Project.js";
-import ScrollAnimation from "../molecules/project2.js";
-import Test2 from "../molecules/test2.js";
 import Layers from "../molecules/LayersSection.js";
 import Head from "../molecules/Head.js";
-import HorizontalScroll from "../molecules/horizontalLayers.js";
-import Projects from "../organisms/Projects.js";
-import Carousel from "../organisms/carousel.js";
 
 function Home (){
 
